e15e45a6b84b0dfd90766417f55b9bc668c25b99
Paolo Di Lorenzo <dilorenzopl@gmail.com>
2017-06-18 20:33:08 -0400

    Test new deployment pipeline

---
+++ scripts/deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+if [[ $TRAVIS_BRANCH == 'master' ]] ; then
+
+  rev = $(git rev-parse --short HEAD)
+  
+  cd _site
+  git init
+
+  git config user.name "Paolo Di Lorenzo (Travis CI)"
+  git config user.email "dilorenzopl@gmail.com"
+
+  git add -A .
+  git commit -m "Deploy pages at ${rev}"
+
+  # We redirect any output to
+  # /dev/null to hide any sensitive credential data that might otherwise be exposed.
+  git push --force --quiet "https://${git_token}@${git_target}" master:master > /dev/null 2>&1
+else
+  echo 'Invalid branch. You can only deploy from master.'
+  exit 1
+fi
