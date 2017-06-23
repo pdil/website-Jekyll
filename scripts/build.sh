4ff2a5db2cf295a1e5684bafb2b02d1b19a662d7
Paolo Di Lorenzo <paolo@dilorenzo.pl>
2017-06-23 11:38:35 -0400

    Fix git config
    Config was being set on external directory instead of _site repo. Changed to `--global` to fix this.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Clone the _site repo so that changes are tracked when the build is pushed back
-git clone --quiet "https://${git_token}@${git_target}"
+git clone --quiet "https://${git_token}@${git_target}" master:master > /dev/null 2>&1
 
-git config user.name "Paolo Di Lorenzo (Travis CI)"
-git config user.email "dilorenzopl@gmail.com"
+git config --global user.name "Paolo Di Lorenzo (Travis CI)"
+git config --global user.email "dilorenzopl@gmail.com"
 
 # Build site and check HTML for errors
 jekyll build
