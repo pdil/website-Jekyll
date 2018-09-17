50d68976169348cd1466116801dec6796e844f98
Paolo Di Lorenzo <dilorenzopl@gmail.com>
2018-09-17 13:31:45 -0400

    Ignore micro.blog urls in htmlproofer
    This would cause errors when posting new entries that have not gone
    live yet.

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,4 +9,4 @@ jekyll build
 # Check for HTML errors
 # (iopscience.iop.org fails for an unknown reason so it is ignored)
 # TODO: add image alts
-htmlproofer ./_site --url-ignore '/.*iopscience.iop.org.*/'
+htmlproofer ./_site --url-ignore '/.*iopscience.iop.org.*/' --url-ignore '/.*micro.blog.*/'
